253915b9b6ee10636e81de37f6de3796c28484e1
Gus Ramírez <g.ram.bt@hotmail.com>
2020-10-11 12:44:28 -0500

    change everything

--- src/frontend/home/index.ts
+++
@@ -1,8 +0,0 @@
-import './styles.css';
-import { clickListener, submitListener } from './globalListener';
-
-const body = document.getElementById('global') as HTMLBodyElement;
-const form = document.getElementById('form__index') as HTMLFormElement;
-
-body.addEventListener('click', clickListener);
-form.addEventListener('submit', submitListener);
--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,8 @@
 import app from './app';
-
-// Implementing Webpack for development build frontend js/css
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
-  const webpack = require('webpack');
-  const compiler = webpack(require('../config/webpack.config.dev.js'));
-
-  compiler.watch(
-    {
-      // Example watchOptions
-      aggregateTimeout: 300,
-      poll: undefined,
-    },
-    (err: any, stats: any) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      console.log(stats.toJson('minimal'));
-    }
-  );
 }
-// Implementing Webpack for development build frontend js/css
 
-const port = process.env.PORT! || 3000;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => console.log(`Server listening on port: ${port}`));
