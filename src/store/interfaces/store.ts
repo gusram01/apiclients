f2bd289662aa637c4c20406779dd39e578c03a09
Gus Ramírez <g.ram.bt@hotmail.com>
2020-10-14 20:39:43 -0500

    more comments

--- src/store/interfaces/store.ts
+++ src/store/interfaces/store.ts
@@ -3,7 +3,9 @@ import { Model } from 'mongoose';
 export interface IStore {
   getAll: () => Promise<Model<any, {}>[]>;
   getOne: (id: string) => Promise<Model<any>>;
+  //@ts-expect-error
   newUser: (user: Partial<T>) => Promise<Model<T>>;
+  //@ts-expect-error
   updateUser: (id: string, user: Partial<T>) => Promise<Model<T>>;
   delUser: (id: string) => Promise<Model<any>>;
 }
