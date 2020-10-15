e8a18a2d6449d0922c8660fb0cea2740d8f4e3a0
Gus Ramírez <g.ram.bt@hotmail.com>
2020-10-14 20:34:08 -0500

    fix issues

--- src/api/components/login/controller.ts
+++ src/api/components/login/controller.ts
@@ -1,12 +1,10 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import crypto from 'crypto';
-import { NextFunction, Request, RequestHandler } from 'express';
-import { err400 } from '../../../middleware/errorResponse';
-import { ErrorResponse } from '../../../middleware/errorResponse';
-import mongoose, { Mongoose } from 'mongoose';
+import { NextFunction } from 'express';
+import mongoose from 'mongoose';
 import { Users } from '../users/users.interface';
-import { Error } from 'mongoose';
+import { ErrorResponse } from '../../../network/response';
 
 const Controller = (injectedModel: mongoose.Model<Users>) => {
   const encrypter = async (password: string) => {
@@ -29,35 +27,35 @@ const Controller = (injectedModel: mongoose.Model<Users>) => {
       const doc = await injectedModel
         .findOne({ email: email })
         .select('password');
-      if (!doc) throw next(err400);
+      if (!doc) throw new ErrorResponse(400, 'Bad Request');
       const validacion = await bcrypt.compare(password, doc.password);
 
       return !validacion ? validacion : doc._id;
     } catch (error) {
-      throw next(error);
+      throw new ErrorResponse(400, error.message);
     }
   };
 
   const findTokenTemporal = async (token: string, next: NextFunction) => {
     try {
       const doc = await injectedModel.findOne({ resetPassword: token });
-      if (!doc) throw next(new ErrorResponse(403, 'The request expires'));
+      if (!doc) throw new ErrorResponse(403, 'Token expires');
 
       const now = new Date(Date.now());
 
       if (!doc.resetPaswordExpire) {
         await doc.set('resetPassword', undefined).save();
         await doc.set('resetPaswordExpire', undefined).save();
-        throw next(new ErrorResponse(403, 'Token expires'));
+        throw new ErrorResponse(403, 'Token expires');
       }
       if (doc.resetPaswordExpire < now) {
         await doc.set('resetPassword', undefined).save();
         await doc.set('resetPaswordExpire', undefined).save();
-        throw next(new ErrorResponse(403, 'Token expires'));
+        throw new ErrorResponse(403, 'Token expires');
       }
       return doc;
     } catch (error) {
-      throw next(error);
+      throw new ErrorResponse(500, error.message);
     }
   };
 
