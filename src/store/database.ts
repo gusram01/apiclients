f68a4b9ef36685d04431d4aec5de6b48e9ad6af4
Gus Ramírez <g.ram.bt@hotmail.com>
2020-10-28 17:23:38 -0600

    repair

--- src/store/database.ts
+++ src/store/database.ts
@@ -9,15 +9,5 @@ const pgp = PGP({
   },
 });
 const db = pgp(connectionString);
-const qf = new QueryFile('file.sql', {
-  minify: true,
-});
-if (qf.error) {
-  throw new ErrorResponse(500, qf.error.message);
-}
-
-db.none(qf)
-  .then(() => console.log('DB ready'))
-  .catch(console.log);
 
 export default db;
